33b0652b897abdbb7a0466fc34848c889a06e47f
Daksh Shah <dakshah@ucsc.edu>
2024-04-13 21:54:01 -0700

    Add size functionality

--- asg1/src/ColoredPoints.js
+++ asg1/src/ColoredPoints.js
@@ -2,9 +2,10 @@
 // Vertex shader program
 var VSHADER_SOURCE = `
   attribute vec4 a_Position;
+  uniform float u_Size;
   void main() {
     gl_Position = a_Position;
-    gl_PointSize = 10.0;
+    gl_PointSize = u_Size;
   }`
 
 // Fragment shader program
@@ -20,6 +21,11 @@ let canvas;
 let gl;
 let a_Position;
 let u_FragColor;
+let u_Size;
+
+let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+let g_selectedSize = 10.0;
+let g_selectedShape = "square";
 
 function setUpWebGL() {
   // Retrieve <canvas> element
@@ -53,22 +59,56 @@ function connectVariablesToGLSL() {
     console.log('Failed to get the storage location of u_FragColor');
     return;
   }
+
+  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+  if(!u_Size) {
+    console.log('Failed to get the storage location of u_Size');
+    return;
+  }
 }
+
+function addActionListeners() {
+  // button events
+
+  // slider events
+  document.getElementById('red-slider').addEventListener('mouseup', function() {g_selectedColor[0] = this.value/255; });
+  document.getElementById('green-slider').addEventListener('mouseup', function() {g_selectedColor[1] = this.value/255; });
+  document.getElementById('blue-slider').addEventListener('mouseup', function() {g_selectedColor[2] = this.value/255; });
+
+  document.getElementById('shape-size-slider').addEventListener('mouseup', function() {g_selectedSize = this.value; })
+}
+
+class Point {
+  constructor() {
+    this.type = "point";
+    this.position = [0.0, 0.0, 0.0];
+    this.color = [1.0, 1.0, 1.0, 1.0];
+
+  }
+}
+
 
 var g_points = [];  // The array for the position of a mouse press
 var g_colors = [];  // The array to store the color of a point
+var g_sizes = [];
+var g_shapes = [];
 function handleClicks(ev) {
+  // let r = document.getElementById('red-slider').value;
+  // let g = document.getElementById('green-slider').value;
+  // let b = document.getElementById('blue-slider').value;
+  // addActionListeners();
+
   let [x, y] = convertMouseToEventCoords(ev);
-  let r = document.getElementById('red-slider').value;
-  let g = document.getElementById('green-slider').value;
-  let b = document.getElementById('blue-slider').value;
+  // gl_point_size = document.getElementById('shape-size-slider').value;
 
   // Store the coordinates to g_points array
   g_points.push([x, y]);
 
   // Store the colors to g_colors array
-  g_colors.push([r/100, g/100, b/100, 1.0]);
+  g_colors.push(g_selectedColor.slice());
 
+  // Store the size to g_sizes array
+  g_sizes.push(g_selectedSize);
   // if (x >= 0.0 && y >= 0.0) {      // First quadrant
   //   g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
   // } else if (x < 0.0 && y < 0.0) { // Third quadrant
@@ -99,11 +139,15 @@ function renderAllShapes() {
   for(var i = 0; i < len; i++) {
     var xy = g_points[i];
     var rgba = g_colors[i];
+    var size = g_sizes[i];
 
     // Pass the position of a point to a_Position variable
     gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
     // Pass the color of a point to u_FragColor variable
     gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+    gl.uniform1f(u_Size, size);
+
     // Draw
     gl.drawArrays(gl.POINTS, 0, 1);
   }
@@ -117,6 +161,8 @@ function main() {
   connectVariablesToGLSL();
 
   canvas.onmousedown = handleClicks //function(ev){ click(ev, gl, canvas, a_Position, u_FragColor) };
+
+  addActionListeners();
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
