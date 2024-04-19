2c83e93edd6c48b250908a041bca8bd56ed4cdb7
Daksh Shah <dakshah@ucsc.edu>
2024-04-18 23:36:08 -0700

    add models

--- asg5a/src/asg5a.js
+++ asg5a/src/asg5a.js
@@ -1,16 +1,27 @@
 import * as THREE from 'three';
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 
 let canvas;
 let renderer;
 let scene;
 let camera;
+let loader;
+let objLoader;
+let controls;
 
 function setUpScene() {
     canvas = document.querySelector('#c');
+
     scene = new THREE.Scene();
+    scene.background = new THREE.Color( 'black' );
+
+    loader = new THREE.TextureLoader();
+    objLoader = new OBJLoader();
 
     renderer = new THREE.WebGLRenderer({antialias: true, canvas});
-    // renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth, window.innerHeight );
     // document.body.appendChild( renderer.domElement );
 
     const fov = 75;
@@ -26,24 +37,32 @@ function setUpScene() {
     light.position.set(-1, 2, 4);
     scene.add(light);   
 
+    controls = new OrbitControls( camera, canvas );
+	controls.target.set( 0, 1, 0 );
+	controls.update();
+
 }
 
-// function createCube(boxWidth = 1, boxHeight = 1, boxDepth = 1, c = 0x44aa88) {
-//     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-//     const material = new THREE.MeshBasicMaterial({color: c});
-//     const cube = new THREE.Mesh(geometry, material);
+function loadColorTexture( path ) {
+    const texture = loader.load( path );
+    texture.colorSpace = THREE.SRGBColorSpace;
+    return texture;
+}
 
-//     scene.add(cube);
-//     renderer.render(scene, camera);
-//     return cube;
-// }
+function createMaterials(files) {
+    let materials = [];
+    for(let i = 0; i < files.length; i++) {
+        materials.push(new THREE.MeshBasicMaterial({map: loadColorTexture(files[i])}));
+    }
+    return materials;
+}
 
-
-// requestAnimationFrame(render);
-function makeInstance(geometry, color, x) {
-    const material = new THREE.MeshPhongMaterial({color});
+function makeInstance(geometry, files, x) {
+    // const texture = loader.load( './../textures/dab.jpg' );
+    // texture.colorSpace = THREE.SRGBColorSpace;
+    let materials = createMaterials(files);
    
-    const cube = new THREE.Mesh(geometry, material);
+    const cube = new THREE.Mesh(geometry, materials);
     scene.add(cube);
    
     cube.position.x = x;
@@ -60,13 +79,17 @@ function main() {
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
     // const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+    let files = ["./../textures/Minecraft/dirt.webp", "./../textures/Minecraft/dirt.webp", "./../textures/Minecraft/dirt.webp", "./../textures/Minecraft/dirt.webp", "./../textures/Minecraft/dirt.webp", "./../textures/Minecraft/dirt.webp"];
 
     const cubes = [
-        makeInstance(geometry, 0x44aa88,  0),
-        makeInstance(geometry, 0x8844aa, -2),
-        makeInstance(geometry, 0xaa8844,  2),
+        // makeInstance(geometry, files,  0),
+        // makeInstance(geometry, files, -2),
+        // makeInstance(geometry, files,  2),
     ];
     
+    objLoader.load('./../textures/models/car.obj', (root) => {
+        scene.add(root);
+    });
 
     function render(time) {
         time *= 0.001;  // convert time to seconds
